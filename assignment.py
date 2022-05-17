2dbe1af7ca0144be9bba4f41b42dece90c2dc9c5
graceo22 <105427363+graceo22@users.noreply.github.com>
2022-05-17 19:20:28 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -8,7 +8,7 @@ st.write("""
 This app predicts the **Iris flower** type!
 """)
 
-st.image("https://s3.amazonaws.com/assets.datacamp.com/blog_assets/Machine+Learning+R/iris-machinelearning.png")
+st.image("https://www.embedded-robotics.com/wp-content/uploads/2022/01/Iris-Dataset-Classification.png")
 
 st.sidebar.header('Input your parameters:')
 
